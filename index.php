b70d86ed52b46bf45bb7ac561dd86c46f4f38751
jeffersantos <jeffersantos@gmail.com>
2020-05-11 22:47:40 -0300

    Fixed variable php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$banco = $_ENV['MYSQL_DATABASE'];
-$usuario = $_ENV['MYSQL_USER'];
-$senha = $_ENV['MYSQL_PASSWORD'];
-$hostname = $_ENV['MYSQL_SERVICE_HOST'];
+$banco = getenv('MYSQL_DATABASE');
+$usuario = getenv('MYSQL_USER');
+$senha = getenv('MYSQL_PASSWORD');
+$hostname = getenv('MYSQL_SERVICE_HOST');
 $conn = mysql_connect($hostname,$usuario,$senha); mysql_select_db($banco) or die( “Não foi possível conectar ao banco MySQL”);
 if (!$conn) {echo “Não foi possível conectar ao banco MySQL.
 “; exit;}
